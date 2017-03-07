0b34814ac83395ec4df823b2143ac91ebdbde164
pablocasares <lito92vforce@gmail.com>
2017-03-07 12:49:50 +0100

    MVP-54 Changed netcat to 200 OK HTTP status

--- historical/historicalOk.sh
+++ historical/historicalOk.sh
@@ -1,8 +1,7 @@
-HISTORICAL_CHECK_PORT=${HISTORICAL_PORT:-8083}
-nc -z 127.0.0.1 $HISTORICAL_CHECK_PORT
-OK=$?
-if [ $OK == 0 ]; then
-        exit 0
+STORICAL_CHECK_PORT=${HISTORICAL_PORT:-8083}
+OK=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${HISTORICAL_CHECK_PORT}/status)
+if [ "$OK" == "200" ]; then
+   exit 0
 else
-        exit 1
+   exit 1
 fi
