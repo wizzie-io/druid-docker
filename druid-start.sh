c4dae38fa5c6976316b83ef68b376e29399845d6
Juan Jesus Chorro Bergillo <jjchorrobe@gmail.com>
2017-05-18 12:46:38 +0200

    Add line for processing jets3t.properties_env with envsubst

--- druid-start.sh
+++ druid-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 envsubst < /opt/druid/conf/_common/common.runtime.properties_env > /opt/druid/conf/_common/common.runtime.properties
 envsubst < /opt/druid/conf/_common/log4j2.xml_env > /opt/druid/conf/_common/log4j2.xml
+envsubst < /opt/druid/conf/_common/jets3t.properties_env > /opt/druid/conf/_common/jets3t.properties
 envsubst < /opt/druid/conf/${DRUID_SERVICE}/runtime.properties_env > /opt/druid/conf/${DRUID_SERVICE}/runtime.properties
-
 java ${DRUID_JVM_ARGS} -cp /opt/druid/conf/_common:/opt/druid/conf/${DRUID_SERVICE}:/opt/druid/lib/* io.druid.cli.Main server ${DRUID_SERVICE}
